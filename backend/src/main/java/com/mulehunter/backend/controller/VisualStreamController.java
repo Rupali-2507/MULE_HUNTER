249a7065a7bb7ab447cbc09779e799e3466f86d9
Rupali2507 <rupalik.ug23.ma@nitp.ac.in>
2025-12-30 21:19:08 +0530

    changed_url

--- backend/src/main/java/com/mulehunter/backend/controller/VisualStreamController.java
+++ backend/src/main/java/com/mulehunter/backend/controller/VisualStreamController.java
@@ -11,32 +11,32 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api/visual")
 public class VisualStreamController {
 
-    private final WebClient webClient;
+        private final WebClient webClient;
 
-    @Value("${visual.internal-api-key}")
-    private String internalApiKey;
+        @Value("${visual.internal-api-key}")
+        private String internalApiKey;
 
-    public VisualStreamController(WebClient.Builder builder) {
-        this.webClient = builder
-                .baseUrl("http://13.61.143.100:8000") // FastAPI base
-                .build();
-    }
+        public VisualStreamController(WebClient.Builder builder) {
+                this.webClient = builder
+                                .baseUrl("http://13.61.143.100:8000") // FastAPI base
+                                .build();
+        }
 
-    @GetMapping(value = "/stream/unsupervised", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> streamUnsupervised(
-            @RequestParam String transactionId,
-            @RequestParam Long nodeId) {
-        System.out.println(
-                "➡ SSE proxy → tx=" + transactionId + " node=" + nodeId);
+        @GetMapping(value = "/stream/unsupervised", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+        public Flux<String> streamUnsupervised(
+                        @RequestParam String transactionId,
+                        @RequestParam Long nodeId) {
+                System.out.println(
+                                "➡ SSE proxy → tx=" + transactionId + " node=" + nodeId);
 
-        return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/visual-analytics/api/visual/stream/unsupervised")
-                        .queryParam("transactionId", transactionId)
-                        .queryParam("nodeId", nodeId)
-                        .build())
-                .header("X-INTERNAL-API-KEY", internalApiKey)
-                .retrieve()
-                .bodyToFlux(String.class);
-    }
+                return webClient.get()
+                                .uri(uriBuilder -> uriBuilder
+                                                .path("/visual-analytics/api/visual/stream/unsupervised")
+                                                .queryParam("transactionId", transactionId)
+                                                .queryParam("nodeId", nodeId)
+                                                .build())
+                                .header("X-INTERNAL-API-KEY", internalApiKey)
+                                .retrieve()
+                                .bodyToFlux(String.class);
+        }
 }
